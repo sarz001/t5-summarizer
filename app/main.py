6d3236b2b7c8d3b0de174a9872b0cddbb15f2f9b
Sazid Ali <sazidalir@gmail.com>
2025-11-06 19:33:43 +0530

    Enable CORS support

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.summarizer import summarize
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="T5 Text Summarizer API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # Allow all origins (or specify domains)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Request(BaseModel):
     text: str
